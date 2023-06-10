b1225084800bcffd884521444984cf6f9faeea64
A normal human <122332042+Ant767@users.noreply.github.com>
2023-06-10 08:52:23 -0500

    Sorry ZSStudios ruined the original commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,30 @@
-hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash hey trash 
-dont worry code backup in vasdcvsda
+import {world, System} from '@minecraft/server';
+
+
+// small utility function lmao
+function isAdmin(player) {
+    return player.isOp() || player.hasTag("admin");
+}
+
+// useful if the function name wasnt so fucking long
+function getAllStringsStartingWithPrefixAndRemovePrefix(list, prefix) {
+    return list
+        .filter(_=>_.startsWith(prefix))
+        .map(_=>_.substring(prefix.length));
+}
+
+let prefix = '!';
+
+world.beforeEvents.chatSend.subscribe(msg=>{
+    msg.cancel = true;
+    if(msg.message.startsWith(prefix)) {
+        world.sendMessage("Need to make commands, sorry. Also this will be better than BaconChat");
+    } else {
+        // chat ranks are done
+        // it just took a shitty utility function and weird string formatting
+        let tags = msg.sender.getTags();
+        let ranks = getAllStringsStartingWithPrefixAndRemovePrefix(tags, "rank:");
+        world.sendMessage(`[${ranks.join('§r, ')}§r] <${msg.sender.nameTag}> ${msg.message}`);
+        return;
+    }
+})
